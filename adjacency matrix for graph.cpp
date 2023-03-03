a8d368c095dc9c6d4f99c98caa09fad11c81b39d
Sunil Kumar Chakravartty <91359079+sunil2010040@users.noreply.github.com>
2023-03-03 23:03:19 +0530

    Add files via upload

---
+++ adjacency matrix for graph.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;    //n=no of nodes, m=no of edges
+    cin>>n>>m;
+    
+    vector<vector<int>> adjm(n+1,vector<int>(n+1, 0)); 
+    
+    for(int i=0;i<m;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        
+        adjm[x][y]=1;
+        adjm[y][x]=1;
+        
+    }
+    
+    cout<<"adjacency matrix of above graph:"<<endl;
+    
+    for(int i=1;i<n+1;i++)
+    {
+        for(int j=1;j<m+1;j++)
+        {   
+            cout<<adjm[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
